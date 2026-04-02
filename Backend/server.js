90a6338744e4c806df58600386570adb8f50e7c7
Nguyen-TrongPhuc <phucgau1508@gmail.com>
2026-04-03 01:24:53 +0700

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 const app = require("./app");
 const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
+const initLoyaltyCron = require("./loyalty.cron");
 
 async function startServer() {
     try {
         // Kết nối đến MongoDB trước khi server bắt đầu lắng nghe
         await MongoDB.connect(config.db.uri);
         console.log("Connected to the database!");
+
+        // Khởi động các tác vụ chạy ngầm (CronJobs)
+        initLoyaltyCron();
 
         const PORT = config.app.port;
         app.listen(PORT, () => {
